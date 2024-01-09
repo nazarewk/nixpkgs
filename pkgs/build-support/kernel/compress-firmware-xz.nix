8cd1149914d36f2bf345b7cb0494f2b209fc6ed1
Krzysztof Nazarewski <gpg@kdn.im>
2024-01-09 12:07:53 +0100

    compressFirmwareXz: never compress lib/firmware/edid
    this allows us not to pass `compressFirmware = false;`
     inside EDID derivations as this mechanic very tricky to discover.
    
    related to #279789

--- pkgs/build-support/kernel/compress-firmware-xz.nix
+++ pkgs/build-support/kernel/compress-firmware-xz.nix
@@ -7,14 +7,18 @@ let
 in
 
 runCommand "${firmware.name}-xz" args ''
+  edids=(-path 'lib/firmware/edid/*')
   mkdir -p $out/lib
   (cd ${firmware} && find lib/firmware -type d -print0) |
-      (cd $out && xargs -0 mkdir -v --)
-  (cd ${firmware} && find lib/firmware -type f -print0) |
+      (cd $out && xargs -0 mkdir -pv --)
+  (cd ${firmware} && find lib/firmware -type f -not "''${edids[@]}" -print0) |
       (cd $out && xargs -0rtP "$NIX_BUILD_CORES" -n1 \
           sh -c 'xz -9c -T1 -C crc32 --lzma2=dict=2MiB "${firmware}/$1" > "$1.xz"' --)
-  (cd ${firmware} && find lib/firmware -type l) | while read link; do
+  (cd ${firmware} && find lib/firmware -type l -not "''${edids[@]}") | while read link; do
       target="$(readlink "${firmware}/$link")"
       ln -vs -- "''${target/^${firmware}/$out}.xz" "$out/$link.xz"
   done
+  (cd ${firmware} && find lib/firmware -type f "''${edids[@]}" -print0) |
+      (cd $out && xargs -0rtP "$NIX_BUILD_CORES" -n1 \
+          sh -c 'ln -vs -- "${firmware}/$1" "$out/$1"' --)
 ''
